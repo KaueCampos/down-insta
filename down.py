21ec08edf5ef42f1a6f317c9163fe1ea56bdb821
Kaue Campos <kauecampos01@hotmail.com>
2022-01-14 10:22:01 -0300

    upando arquivos

---
+++ down.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    --------------------------------------------------------------------------------------------------------------------
+    Description:
+    --------------------------------------------------------------------------------------------------------------------
+    Author:           @kaue.oliveira__
+    Last Update:      14/01/2022 10:13
+    Created:          14/01/2022 10:13
+    Copyright:        (c) Ampere Consultoria Ltda
+    Arquivo:          down
+    Original Project: PyCharm
+    IDE:              PyCharm
+"""
+from instabot import bot
+
+bot = bot()
+
+bot.login(username='',
+          password='')
+
+bot.upload_photo("",
+                 caption="")
+
+
+
+
